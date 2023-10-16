16b9935e3f4c39608d5b77c75a74688871c34743
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-16 17:10:04 -0300

    Atualização ref. seção 3 - aula 13 - exercício extra 4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -31,10 +31,21 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.error').should('be.visible')
     })
 
-    it.only('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
         cy.get('#phone')
             .type('abcdefghij')
             .should('have.value', '')
-    })    
+    })
+    
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Gilson')
+        cy.get('#lastName').type('Silva')
+        cy.get('#email').type('gilsongs.1979@gmail.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()        
+        
+        cy.get('.error').should('be.visible')    
+    })      
   })
   
