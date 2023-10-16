26ad4f156d3a46c4a66e8a4cba1b090c28a5ec7a
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-16 16:56:18 -0300

    Atualização ref. seção 3 - aula 11 - exercício extra 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    it('preenche os campos obrigatórios e envia o formulário', function() {
         const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
         
         cy.get('#firstName').type('Gilson')
@@ -19,6 +19,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
+    })
+
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('#firstName').type('Gilson')
+        cy.get('#lastName').type('Silva')
+        cy.get('#email').type('gilsongs.1979@gmail,com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+        
+        cy.get('.error').should('be.visible')
     })
   })
   
