a07e45c2eda6094b080dfbc40a0ef7a020ceaf10
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-16 16:52:41 -0300

    Atualização ref. seção 3 - aula 10 - exercício extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,10 +10,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
+        
         cy.get('#firstName').type('Gilson')
         cy.get('#lastName').type('Silva')
         cy.get('#email').type('gilsongs.1979@gmail.com')
-        cy.get('#open-text-area').type('Teste')
+        cy.get('#open-text-area').type(longText, { delay: 0 })
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
