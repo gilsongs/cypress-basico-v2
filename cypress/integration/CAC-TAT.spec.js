4edd3f55eab4eb711efdace51edc94af62569fb2
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-22 13:55:36 -0300

    Atualização ref. seção 4 - aula 26 - exercício

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -119,9 +119,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it.only('marca ambos checkboxes, depois desmarca o último', function() {
         cy.get('input[type="checkbox"]')
             .check()
+            .should('be.checked')
             .last().uncheck()
             .should('not.be.checked')
 
-    })       
+    })  
+    
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+
+    })     
   })
   
