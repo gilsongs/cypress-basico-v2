d1d229768c3be09a9931929c4c13d9466ec16936
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-22 13:18:49 -0300

    Atualização ref. seção 4 - aula 23 - exercício

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -89,10 +89,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('have.value', 'youtube')
     }) 
     
-    it.only('seleciona um produto (Mentoria) por seu valor (value)', function() {
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
         cy.get('#product')
             .select('mentoria')
             .should('have.value', 'mentoria')
+    })
+    
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+    
+    it.only('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
     })     
   })
   
