8a52123660365eba5efc466bab4e60f8260b0fa2
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-23 14:10:31 -0300

    Removido o 'only' do último teste criado

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -239,7 +239,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             })
     })
 
-    it.only('encontra o gato escondido', function() {
+    it('encontra o gato escondido', function() {
         cy.get('#cat')
             .invoke('show')
             .should('be.visible')
