5bb53b17a424df30c6f3df642bebc8975e21d3e3
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-23 13:58:37 -0300

    Atualização ref. seção 12 - aula 54 - exercício extra 4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -226,6 +226,17 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#open-text-area')
           .invoke('val', longText)
           .should('have.value', longText)
-    })    
+    })   
+    
+    it.only('faz uma requisição HTTP', function() {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response) {
+                // console.log(response)
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
+    })
   })
   
