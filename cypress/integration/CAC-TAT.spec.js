965b9d79c29d0adbd6520b8da2f20205b311a883
Gilson da Silva <gilsongs.1979@gmail.com>
2023-10-23 12:22:26 -0300

    Atualização ref. seção 12 - aula 52 - exercício extra 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -203,7 +203,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('Talking About Testing').should('be.visible')
     })
 
-    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
         cy.get('.success')
         .should('not.be.visible')
         .invoke('show')
@@ -219,5 +219,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .invoke('hide')
         .should('not.be.visible')
     })
+
+    it('preenche a area de texto usando o comando invoke', function() {
+        const longText = Cypress._.repeat('0123456789', 20)
+
+        cy.get('#open-text-area')
+          .invoke('val', longText)
+          .should('have.value', longText)
+    })    
   })
   
